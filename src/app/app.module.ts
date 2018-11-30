bb7cb5194fae99242986e0e9e53e9f92f57a1911
Unknown <xmena97@gmail.com>
2018-11-30 11:15:24 +0100

    1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,20 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDHvXzsNuQJY-r9S4Y41TX-xuWE63o3Vco",
+    authDomain: "catalogomovil-858dc.firebaseapp.com",
+    databaseURL: "https://catalogomovil-858dc.firebaseio.com",
+    projectId: "catalogomovil-858dc",
+    storageBucket: "catalogomovil-858dc.appspot.com",
+    messagingSenderId: "569910277293"
+  };
+
 import { MyApp } from './app.component';
 import {
         PrincipalPage,
@@ -25,8 +39,12 @@ import {
     IonicModule.forRoot(MyApp,{
       backButtonText: 'Volver',
       backButtonIcon: 'walk',
-    })
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
+
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
@@ -38,6 +56,7 @@ import {
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
