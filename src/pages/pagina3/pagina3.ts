c6a50a345dd8502be7b2a9d0435d48bf89c90c16
Unknown <xmena97@gmail.com>
2018-12-12 21:19:01 +0100

    Definitivo

--- src/pages/pagina3/pagina3.ts
+++ src/pages/pagina3/pagina3.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {PrincipalPage,Pagina4Page} from '../index.paginas';
+import {Pagina4Page} from '../index.paginas';
 
 /**
  * Generated class for the Pagina3Page page.
